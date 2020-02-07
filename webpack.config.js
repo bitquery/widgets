d9bce3050b32cf5d1b3137960de04daba2223a23
Andrey <bigzero@yandex.ru>
2020-02-07 13:43:29 +0300

    minimize

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ module.exports = function(paths){
                         {
                             loader: MiniCssExtractPlugin.loader,
                             options: {
-                                publicPath: 'http://localhost:63342/widgets/dist/',
+                                publicPath: 'https://cdn.jsdelivr.net/gh/bitquery/widgets@v1.0.54/',
                                 minimize: true
                             },
                         },
