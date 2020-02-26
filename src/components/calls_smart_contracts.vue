ad6f594250a96e58cb1f205d9677cc083bf36b98
Andrey <bigzero@yandex.ru>
2020-02-26 11:23:49 +0300

    ellipsis on table

--- src/components/calls_smart_contracts.vue
+++ src/components/calls_smart_contracts.vue
@@ -1,22 +1,24 @@
 <template>
     <errors :errors="errors" obj="calls_smart_contracts" func="calls_smart_contracts" :exclude="['csv']" title="Calls Smart Contracts" v-if="error"></errors>
-    <div class="table-responsive" v-else>
-        <table :class="'table table-striped table-hover table-sm widgets-table ' + theme_class">
-            <thead>
-            <tr>
-                <th>{{ $t("title.smart_contract") }}</th>
-                <th>{{ $t("title.method") }}</th>
-                <th class="text-right">{{ $t("title.tx_count") }}</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="item in result">
-                <td><smart-contract :locale="locale" :smartcontract="item.smartContract"></smart-contract></td>
-                <td><smart-contract-method :locale="locale" :smartcontractmethod="item.smartContractMethod"></smart-contract-method></td>
-                <td class="text-right"><call-count :locale="locale" :count="item.count" :address="item.smartContract.address.address"></call-count></td>
-            </tr>
-            </tbody>
-        </table>
+    <div v-else>
+        <div class="table-responsive">
+            <table :class="'table table-striped table-hover table-sm widgets-table ' + theme_class">
+                <thead>
+                <tr>
+                    <th>{{ $t("title.smart_contract") }}</th>
+                    <th>{{ $t("title.method") }}</th>
+                    <th class="text-right">{{ $t("title.tx_count") }}</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="item in result">
+                    <td class="ellipsis"><span><smart-contract :locale="locale" :smartcontract="item.smartContract"></smart-contract></span></td>
+                    <td class="ellipsis"><span><smart-contract-method :locale="locale" :smartcontractmethod="item.smartContractMethod"></smart-contract-method></span></td>
+                    <td class="text-right"><call-count :locale="locale" :count="item.count" :address="item.smartContract.address.address"></call-count></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
         <more-text :count="result.length" :load_count="100" :path="path"></more-text>
         <links obj="calls_smart_contracts" func="calls_smart_contracts" title="Calls Smart Contracts"></links>
     </div>
--- src/components/transfers_by_currencies.vue
+++ src/components/transfers_by_currencies.vue
@@ -1,30 +1,32 @@
 <template>
     <errors :errors="errors" obj="transfers_by_currencies" func="transfers_by_currencies" :exclude="['csv']" title="Transfers By Currencies" v-if="error"></errors>
-    <div class="table-responsive" v-else>
-        <table :class="'table table-striped table-hover table-sm widgets-table ' + theme_class">
-            <thead>
-            <tr>
-                <th>{{ $t("title.currency") }}</th>
-                <th></th>
-                <th class="text-right">{{ $t("title.receive") }}</th>
-                <th class="text-right">{{ $t("title.txs") }}</th>
-                <th class="text-right">{{ $t("title.send") }}</th>
-                <th></th>
-                <th class="text-right">{{ $t("title.txs") }}</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="item in result">
-                <td><token-address :locale="locale" :name="item.currency.symbol" :addr="item.currency.address"></token-address></td>
-                <td class="text-center"><i class="fa fa-sign-in text-success"></i></td>
-                <td class="text-right"><amount-display :amount="item.sum_in"></amount-display></td>
-                <td class="text-right"><transfer-to-address :locale="locale" :count="item.count_in" :currency_id="item.currency.currencyId"></transfer-to-address></td>
-                <td class="text-right"><amount-display :amount="item.sum_out"></amount-display></td>
-                <td class="text-center"><i class="fa fa-sign-out text-warning"></i></td>
-                <td class="text-right"><transfer-from-address :locale="locale" :count="item.count_out" :currency_id="item.currency.currencyId"></transfer-from-address></td>
-            </tr>
-            </tbody>
-        </table>
+    <div v-else>
+        <div class="table-responsive">
+            <table :class="'table table-striped table-hover table-sm widgets-table ' + theme_class">
+                <thead>
+                <tr>
+                    <th>{{ $t("title.currency") }}</th>
+                    <th></th>
+                    <th class="text-right">{{ $t("title.receive") }}</th>
+                    <th class="text-right">{{ $t("title.txs") }}</th>
+                    <th class="text-right">{{ $t("title.send") }}</th>
+                    <th></th>
+                    <th class="text-right">{{ $t("title.txs") }}</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="item in result">
+                    <td class="ellipsis"><span><token-address :locale="locale" :name="item.currency.symbol" :addr="item.currency.address"></token-address></span></td>
+                    <td class="text-center"><i class="fa fa-sign-in text-success"></i></td>
+                    <td class="text-right"><amount-display :amount="item.sum_in"></amount-display></td>
+                    <td class="text-right"><transfer-to-address :locale="locale" :count="item.count_in" :currency_id="item.currency.currencyId"></transfer-to-address></td>
+                    <td class="text-right"><amount-display :amount="item.sum_out"></amount-display></td>
+                    <td class="text-center"><i class="fa fa-sign-out text-warning"></i></td>
+                    <td class="text-right"><transfer-from-address :locale="locale" :count="item.count_out" :currency_id="item.currency.currencyId"></transfer-from-address></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
         <more-text :count="result.length" :load_count="100" :path="path"></more-text>
         <links obj="transfers_by_currencies" func="transfers_by_currencies" title="Transfers By Currencies"></links>
     </div>
