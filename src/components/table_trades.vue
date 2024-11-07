9bfa1bf77f19c57ab797748ebfae3307fbc9618f
Andrey Stepanov <bigzero@yandex.ru>
2024-11-07 10:12:59 +0200

    v1.3.24

--- src/components/table_trades.vue
+++ src/components/table_trades.vue
@@ -109,10 +109,10 @@
                         return _.merge(parameters, data);
                         break;
                     case 'amount':
-                        return _.merge(parameters, {component: 'amount', thClass: 'text-right', tdClass: 'text-right '+ parameters.tdClass}, data);
+                        return _.merge(parameters, {component: 'amount', thClass: 'text-end', tdClass: 'text-end '+ parameters.tdClass}, data);
                         break;
                     case 'count':
-                        return _.merge(parameters, {component: 'count', thClass: 'text-right', tdClass: 'text-right '+ parameters.tdClass}, data);
+                        return _.merge(parameters, {component: 'count', thClass: 'text-end', tdClass: 'text-end '+ parameters.tdClass}, data);
                         break;
                     case 'string':
                     default:
